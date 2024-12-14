a65e7cbeb5a42f0a412329cd4bc3aa55431a0794
Masoud Khoshkam <masoud.khoshkamkhatibgourabi@snapp.cab>
2024-12-14 16:03:25 +0330

    add thank you screen

--- presentation/src/main/java/com/mkdev/presentation/screen/home/HomeScreen.kt
+++ presentation/src/main/java/com/mkdev/presentation/screen/home/HomeScreen.kt
@@ -3,8 +3,6 @@ package com.mkdev.presentation.screen.home
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.mkdev.presentation.screen.home.components.HomeScreenContent
 
@@ -12,9 +10,6 @@ import com.mkdev.presentation.screen.home.components.HomeScreenContent
 internal fun HomeScreen(
     onNavigateToThankYou: () -> Unit,
 ) {
-
-    var backgroundImageUrl = remember { mutableStateOf<String?>(null) }
-
     HomeScreenContent(
         modifier = Modifier
             .fillMaxSize(),
--- presentation/src/main/java/com/mkdev/presentation/screen/thankYou/components/ThankYouScreenContent.kt
+++ presentation/src/main/java/com/mkdev/presentation/screen/thankYou/components/ThankYouScreenContent.kt
@@ -1,19 +1,51 @@
 package com.mkdev.presentation.screen.thankYou.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.mkdev.presentation.R
+import com.mkdev.presentation.theme.Dimens
 
 @Composable
 internal fun ThankYouScreenContent(
     modifier: Modifier,
 ) {
-    Box(
-        modifier = modifier
-            .background(Color.Yellow)
+    Column(
+        modifier = modifier.padding(Dimens.PaddingStandard),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            modifier = Modifier.size(Dimens.ThankYouImageSize),
+            painter = painterResource(R.drawable.img_thanks),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight
+        )
 
+        Spacer(modifier = Modifier.height(Dimens.PaddingStandard))
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = stringResource(R.string.thanks_for_taking_the_survey),
+            color = Color.White,
+            style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
+        )
     }
 }
