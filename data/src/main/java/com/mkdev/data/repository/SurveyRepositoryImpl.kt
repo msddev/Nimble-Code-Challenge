2b2552889d4a1cbccf8d08638d73f925bba93dfd
Masoud Khoshkam <masoud.khoshkamkhatibgourabi@snapp.cab>
2024-12-16 21:32:20 +0330

    implement survey repository by offline and online scenario and paging

--- data/src/main/java/com/mkdev/data/repository/SurveyRepositoryImpl.kt
+++ data/src/main/java/com/mkdev/data/repository/SurveyRepositoryImpl.kt
@@ -1,24 +1,53 @@
 package com.mkdev.data.repository
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.map
+import com.mkdev.data.datasource.local.database.room.dao.SurveyDao
+import com.mkdev.data.datasource.local.database.room.dao.SurveyRemoteKeyDao
+import com.mkdev.data.datasource.local.database.room.mediator.SurveyRemoteMediator
+import com.mkdev.data.datasource.local.mapper.SurveyEntityMapper
 import com.mkdev.data.datasource.remote.api.SurveyApi
-import com.mkdev.data.datasource.remote.mapper.SurveyMapper
-import com.mkdev.domain.entity.survey.SurveyEntity
+import com.mkdev.data.datasource.remote.mapper.SurveyDomainMapper
+import com.mkdev.data.utils.RemoteApiPaging
+import com.mkdev.domain.entity.survey.SurveyModel
 import com.mkdev.domain.repository.SurveyRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+@OptIn(ExperimentalPagingApi::class)
 class SurveyRepositoryImpl @Inject constructor(
     private val surveyApi: SurveyApi,
-    private val surveyMapper: SurveyMapper,
+    private val surveyDao: SurveyDao,
+    private val surveyRemoteKeyDao: SurveyRemoteKeyDao,
+    private val surveyDomainMapper: SurveyDomainMapper,
+    private val surveyEntityMapper: SurveyEntityMapper,
 ) : SurveyRepository {
 
-    override fun getSurveys(): Flow<PagingData<SurveyEntity>> {
-        /*return Pager(
-            config = androidx.paging.PagingConfig(pageSize = 5),
-            pagingSourceFactory = { SurveyPagingSource(surveyApi, surveyMapper) }
-        ).flow*/
+    override fun getSurveys(): Flow<PagingData<SurveyModel>> {
 
-        TODO()
+        val pagingSourceFactory = { surveyDao.getByPaging() }
+
+        return Pager(
+            config = PagingConfig(
+                pageSize = RemoteApiPaging.PAGE_SIZE,
+                maxSize = RemoteApiPaging.PAGE_SIZE + (RemoteApiPaging.PAGE_SIZE * 2),
+                enablePlaceholders = false
+            ),
+            remoteMediator = SurveyRemoteMediator(
+                surveyApi = surveyApi,
+                surveyDao = surveyDao,
+                surveyRemoteKeyDao = surveyRemoteKeyDao,
+                surveyEntityMapper = surveyEntityMapper,
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow.map { pagingData ->
+            pagingData.map { item ->
+                surveyDomainMapper.mapToSurveyDomainModel(item)
+            }
+        }
     }
 }
