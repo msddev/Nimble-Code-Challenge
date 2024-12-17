bd28d98eae7766ec7e4f64d8fca5c94ebc2bb40d
Masoud Khoshkam <masoud.khoshkamkhatibgourabi@snapp.cab>
2024-12-17 10:24:59 +0330

    make pager indicator better

--- presentation/src/main/java/com/mkdev/presentation/screen/home/components/SurveyItemView.kt
+++ presentation/src/main/java/com/mkdev/presentation/screen/home/components/SurveyItemView.kt
@@ -1,6 +1,7 @@
 package com.mkdev.presentation.screen.home.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -19,12 +21,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -34,6 +39,7 @@ import com.mkdev.presentation.common.component.GlideImageLoader
 import com.mkdev.presentation.common.utils.pagerFadeTransition
 import com.mkdev.presentation.theme.CommonColors
 import com.mkdev.presentation.theme.Dimens
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,39 +68,7 @@ internal fun SurveyItemView(
                 .align(Alignment.BottomCenter)
         ) {
 
-            Row(
-                modifier = Modifier
-                    .pagerFadeTransition(page = page, pagerState = pagerState)
-                    .fillMaxWidth()
-                    .padding(bottom = Dimens.PaddingStandard),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                repeat(pagerState.pageCount) { iteration ->
-                    val color: Color
-                    val shape: Shape
-                    val modifier: Modifier
-                    if (pagerState.currentPage == iteration) {
-                        color = Color.DarkGray
-                        shape = RoundedCornerShape(50)
-                        modifier = Modifier.size(
-                            width = Dimens.PagerIndicatorWidth,
-                            height = Dimens.PagerIndicatorHeight
-                        )
-                    } else {
-                        color = Color.LightGray
-                        shape = CircleShape
-                        modifier = Modifier.size(Dimens.PagerIndicatorHeight)
-                    }
-                    Box(
-                        modifier = Modifier
-                            .padding(Dimens.Padding3xSmall)
-                            .clip(shape)
-                            .background(color)
-                            .then(modifier)
-                    )
-                }
-            }
+            PagerIndicator(page = page, pagerState = pagerState)
 
             Text(
                 text = survey.title,
@@ -131,6 +105,60 @@ internal fun SurveyItemView(
                     style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun PagerIndicator(page: Int, pagerState: PagerState) {
+    val coroutineScope = rememberCoroutineScope()
+    val horizontalScrollState = rememberScrollState()
+    val localDensity = LocalDensity.current
+
+    val startIndex = maxOf(0, minOf(pagerState.currentPage - 2, pagerState.pageCount - 5))
+    val endIndex = minOf(pagerState.pageCount - 1, startIndex + 4)
+
+    Row(
+        modifier = Modifier
+            .pagerFadeTransition(page = page, pagerState = pagerState)
+            .fillMaxWidth()
+            .padding(bottom = Dimens.PaddingStandard)
+            .horizontalScroll(horizontalScrollState),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        for (iteration in startIndex..endIndex) {
+            val color: Color
+            val shape: Shape
+            val modifier: Modifier
+            if (pagerState.currentPage == iteration) {
+                color = Color.DarkGray
+                shape = RoundedCornerShape(50)
+                modifier = Modifier.size(
+                    width = Dimens.PagerIndicatorWidth,
+                    height = Dimens.PagerIndicatorHeight
+                )
+            } else {
+                color = Color.LightGray
+                shape = CircleShape
+                modifier = Modifier.size(Dimens.PagerIndicatorHeight)
+            }
+            Box(
+                modifier = Modifier
+                    .padding(Dimens.Padding3xSmall)
+                    .clip(shape)
+                    .background(color)
+                    .then(modifier)
+            )
+        }
+    }
+
+    LaunchedEffect(pagerState.currentPage) {
+        coroutineScope.launch {
+            val scrollPosition = (pagerState.currentPage - startIndex) *
+                    (localDensity.run { Dimens.PagerIndicatorWidth.toPx() } +
+                            localDensity.run { Dimens.Padding3xSmall.toPx() } * 2)
+            horizontalScrollState.animateScrollTo(scrollPosition.toInt())
         }
     }
 }
